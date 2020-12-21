6ea922a26ad951586859d2e0cd49d7f75e633524
marksav85 <mark.saville@outlook.com>
2020-12-21 19:33:24 +0100

    Create program to rename files in folders

---
+++ rename_files.py
@@ -0,0 +1,11 @@
+import os
+import string
+def rename_files():
+    #get filenames from folder
+    file_list = os.listdir(insert folder path)
+    #print (file_list)
+    saved_path = os.getcwd()
+    print ("Current Working Directory is "+saved_path)
+    
+
+
