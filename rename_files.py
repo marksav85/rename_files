2c295c2dac02af2b982a09bfdacf2733e4e91005
marksav85 <mark.saville@outlook.com>
2020-12-23 16:34:40 +0100

    refactor: Add print statement for CWD

--- rename_files.py
+++ rename_files.py
@@ -2,9 +2,11 @@ import os
 import string
 def rename_files():
     #get filenames from folder
-    file_list = os.listdir(insert folder path)
+    file_list = os.listdir(r"D:\OneDrive\Programming and Data Science\Projects\rename_files\pictures")
     #print (file_list)
     saved_path = os.getcwd()
     print ("Current Working Directory is "+saved_path)
+    os.chdir(r"C:\Users\marks\Desktop\Intro to Programming\prank")
+    print (os.getcwd())
     
 
